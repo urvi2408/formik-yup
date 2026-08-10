bf838047fb15b69b742a2cab1269848839def9dc
urvi2408 <bhavsarurvi143@gmail.com>
2026-08-10 16:51:27 +0530

    fix deployment dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from "react";
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 import Admin from "./Admin";
 import User from "./User";
-import Home from "./Home";
 
 function App() {
   return (
